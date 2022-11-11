a4ac6a79ed4425247afea1d587a2c7e47e86eef0
Kaustubh Pande <kaustubhpande.kp@gmail.com>
2022-11-11 15:11:09 +0530

    rename func-socat to func-utils & add tar image (#1161)

--- hack/build.sh.d/kn-plugin-func.sh
+++ hack/build.sh.d/kn-plugin-func.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # There no midstream image currently, fallback to upstream one.
-readonly socat_image="${KN_PLUGIN_FUNC_SOCAT_IMAGE:-quay.io/boson/alpine-socat:1.7.4.3-r1-non-root}"
+readonly func_util_image="${KN_PLUGIN_FUNC_UTIL_IMAGE:-quay.io/boson/alpine-socat:1.7.4.3-r1-non-root}"
 export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
--X knative.dev/kn-plugin-func/k8s.SocatImage=${socat_image} "
+-X knative.dev/kn-plugin-func/k8s.SocatImage=${func_util_image} \
+-X knative.dev/kn-plugin-func/k8s.TarImage=${func_util_image}"
 
