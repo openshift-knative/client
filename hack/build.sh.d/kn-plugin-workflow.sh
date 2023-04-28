8da7111843777e425a1ba5d1d9da03a3141e5f9a
Eder Ignatowicz <ignatowicz@gmail.com>
2023-04-28 10:03:29 -0400

    [release-v1.8] kn-workflow-plugin - update quarkus to downstream version Quarkus bump for 2.13.7.Final-redhat-00003

---
+++ hack/build.sh.d/kn-plugin-workflow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+readonly quarkus_version="2.13.7.SP1-redhat-00001"
+readonly quarkus_platform_group_id="com.redhat.quarkus.platform"
+export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
+-X github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/metadata.QuarkusVersion=${quarkus_version} \
+-X github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/metadata.QuarkusPlatformGroupId=${quarkus_platform_group_id}"
