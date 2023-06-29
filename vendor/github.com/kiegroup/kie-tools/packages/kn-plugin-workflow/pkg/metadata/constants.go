f374ae5cd6148dc46c5847438f638a598e09d5e3
David Simansky <dsimansk@redhat.com>
2023-06-29 15:21:23 +0200

    [release-v1.9] kn-workflow-plugin 0.30.1

---
+++ vendor/github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/metadata/constants.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package metadata
+
+const (
+	QuarkusMavenPlugin                          = "quarkus-maven-plugin"
+	QuarkusKubernetesExtension                  = "quarkus-kubernetes"
+	QuarkusResteasyJacksonExtension             = "quarkus-resteasy-jackson"
+	QuarkusContainerImageJib                    = "quarkus-container-image-jib"
+	SmallryeHealth                              = "smallrye-health"
+	QuarkusContainerImageDocker                 = "quarkus-container-image-docker"
+	KogitoQuarkusServerlessWorkflowExtension    = "kogito-quarkus-serverless-workflow"
+	KogitoAddonsQuarkusKnativeEventingExtension = "kogito-addons-quarkus-knative-eventing"
+	KogitoQuarkusServerlessWorkflowDevUi        = "kogito-quarkus-serverless-workflow-devui"
+	KogitoAddonsQuarkusSourceFiles              = "kogito-addons-quarkus-source-files"
+	KogitoDataIndexInMemory                     = "kogito-addons-quarkus-data-index-inmemory"
+
+	JavaVersion       = 11
+	MavenMajorVersion = 3
+	MavenMinorVersion = 8
+
+	DefaultTag     = "latest"
+	WorkflowSwJson = "workflow.sw.json"
+
+	OperatorName       = "sonataflow-operator-system"
+	OperatorManagerPod = "sonataflow-operator-controller-manager"
+
+	YAMLExtension         = "sw.yaml"
+	YAMLExtensionShort    = "sw.yml"
+	JSONExtension         = "sw.json"
+	ApplicationProperties = "application.properties"
+
+	ManifestServiceFilesKind = "SonataFlow"
+)
