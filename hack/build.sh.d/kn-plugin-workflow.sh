28dcb7ab53349f69acdc1d371b497640134b2326
David Simansky <dsimansk@redhat.com>
2023-06-30 16:40:47 +0200

    Update workflow-plugin quarkus & dev image LD flags

--- hack/build.sh.d/kn-plugin-workflow.sh
+++ hack/build.sh.d/kn-plugin-workflow.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-readonly quarkus_version="2.13.7.SP1-redhat-00001"
+readonly quarkus_version="2.13.8.Final-redhat-00004"
 readonly quarkus_platform_group_id="com.redhat.quarkus.platform"
-readonly dev_mode_image="quay.io/kiegroup/kogito-swf-devmode:1.40"
+readonly dev_mode_image="registry.redhat.io/openshift-serverless-1-tech-preview/logic-swf-devmode-rhel8:1.30"
 export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
 -X github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/metadata.DevModeImage=${dev_mode_image} \
 -X github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/metadata.QuarkusVersion=${quarkus_version} \
