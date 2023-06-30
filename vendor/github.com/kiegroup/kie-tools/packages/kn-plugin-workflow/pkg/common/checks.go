d625e226357fa51adf4343a7c27f9fdab8384738
David Simansky <dsimansk@redhat.com>
2023-06-30 16:37:23 +0200

    Update vendor dir

--- vendor/github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/common/checks.go
+++ vendor/github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/common/checks.go
@@ -19,7 +19,8 @@ package common
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -42,7 +43,7 @@ func checkJava() error {
 	version, err := javaCheck.CombinedOutput()
 	if err != nil {
 		fmt.Println("ERROR: Java not found")
-		fmt.Printf("At least Java %.2d is required to use this command\n", metadata.JAVA_VERSION)
+		fmt.Printf("At least Java %.2d is required to use this command\n", metadata.JavaVersion)
 		return err
 	}
 	userJavaVersion, err := parseJavaVersion(string(version))
@@ -50,8 +51,8 @@ func checkJava() error {
 		return fmt.Errorf("error while parsing Java version: %w", err)
 	}
 
-	if userJavaVersion < metadata.JAVA_VERSION {
-		fmt.Printf("ERROR: Please make sure you are using Java version %.2d or later", metadata.JAVA_VERSION)
+	if userJavaVersion < metadata.JavaVersion {
+		fmt.Printf("ERROR: Please make sure you are using Java version %.2d or later", metadata.JavaVersion)
 		fmt.Println("Installation stopped. Please upgrade Java and run again")
 		os.Exit(1)
 	} else {
@@ -65,7 +66,7 @@ func checkMaven() error {
 	version, err := mavenCheck.CombinedOutput()
 	if err != nil {
 		fmt.Println("ERROR: Maven not found")
-		fmt.Printf("At least Maven %.2d.%.2d.1 is required to use this command\n", metadata.MAVEN_MAJOR_VERSION, metadata.MAVEN_MINOR_VERSION)
+		fmt.Printf("At least Maven %.2d.%.2d.1 is required to use this command\n", metadata.MavenMajorVersion, metadata.MavenMinorVersion)
 		return err
 	}
 	major, minor, err := parseMavenVersion(string(version))
@@ -73,7 +74,7 @@ func checkMaven() error {
 		return fmt.Errorf("error while parsing Maven version: %w", err)
 	}
 
-	if major < metadata.MAVEN_MAJOR_VERSION && minor < metadata.MAVEN_MINOR_VERSION {
+	if major < metadata.MavenMajorVersion && minor < metadata.MavenMinorVersion {
 		fmt.Printf("ERROR: Please make sure you are using Maven version %d.%d.1 or later", major, minor)
 		fmt.Println("Installation stopped. Please upgrade Maven and run again")
 		os.Exit(1)
@@ -112,21 +113,6 @@ func CheckPodman() error {
 	return nil
 }
 
-func CheckKubectl() error {
-	fmt.Println("✅ Checking if kubectl is available...")
-	_, kubectlCheck := exec.LookPath("kubectl")
-	if err := kubectlCheck; err != nil {
-		fmt.Println("ERROR: kubectl not found")
-		fmt.Println("kubectl is required for deploy")
-		fmt.Println("Download from https://kubectl.docs.kubernetes.io/installation/kubectl/")
-		os.Exit(1)
-		return err
-	}
-
-	fmt.Println(" - kubectl is available")
-	return nil
-}
-
 func parseJavaVersion(version string) (int64, error) {
 	dotVersion := strings.Split(strings.Split(version, "\"")[1], ".")
 	intVersion, err := strconv.ParseInt(dotVersion[0], 10, 8)
@@ -159,4 +145,45 @@ func CheckIfDirExists(dirName string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func IsQuarkusSonataFlowProject() bool {
+	if fileExists("pom.xml") {
+		return true
+	}
+	return false
+}
+
+func IsSonataFlowProject() bool {
+	if anyFileExists("*.sw.*") {
+		return true
+	}
+	return false
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func anyFileExists(extension string) bool {
+	matches, err := filepath.Glob(extension)
+	if err != nil {
+		return false
+	}
+
+	if len(matches) > 0 {
+		return true
+	}
+	return false
+}
+
+func CheckProjectName(name string) (err error) {
+	matched, err := regexp.MatchString(`^([_\-\.a-zA-Z0-9]+)$`, name)
+	if !matched {
+		fmt.Printf("The project name (\"%s\") contains invalid characters. Valid characters are alphanumeric (A-Za-z), underscore, dash and dot.", name)
+		err = fmt.Errorf("invalid project name")
+
+	}
+	return
 }
---
+++ vendor/github.com/serverlessworkflow/sdk-go/v2/validator/validator.go
@@ -0,0 +1,74 @@
+// Copyright 2021 The Serverless Workflow Specification Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validator
+
+import (
+	"context"
+
+	validator "github.com/go-playground/validator/v10"
+	"github.com/senseyeio/duration"
+)
+
+// TODO: expose a better validation message. See: https://pkg.go.dev/gopkg.in/go-playground/validator.v8#section-documentation
+
+type Kind interface {
+	KindValues() []string
+	String() string
+}
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+
+	err := validate.RegisterValidationCtx("iso8601duration", validateISO8601TimeDurationFunc)
+	if err != nil {
+		panic(err)
+	}
+
+	err = validate.RegisterValidation("oneofkind", oneOfKind)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+// GetValidator gets the default validator.Validate reference
+func GetValidator() *validator.Validate {
+	return validate
+}
+
+// ValidateISO8601TimeDuration validate the string is iso8601 duration format
+func ValidateISO8601TimeDuration(s string) error {
+	_, err := duration.ParseISO8601(s)
+	return err
+}
+
+func validateISO8601TimeDurationFunc(_ context.Context, fl validator.FieldLevel) bool {
+	err := ValidateISO8601TimeDuration(fl.Field().String())
+	return err == nil
+}
+
+func oneOfKind(fl validator.FieldLevel) bool {
+	if val, ok := fl.Field().Interface().(Kind); ok {
+		for _, value := range val.KindValues() {
+			if value == val.String() {
+				return true
+			}
+		}
+	}
+
+	return false
+}
