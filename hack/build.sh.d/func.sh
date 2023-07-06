33450bb9dadb9f592f46f6ccdccadb5867dfaf46
Kaustubh Pande <kaustubhpande.kp@gmail.com>
2023-07-06 17:28:08 +0530

    [SRVOCF-477] add default s2i builders

--- hack/build.sh.d/func.sh
+++ hack/build.sh.d/func.sh
@@ -2,6 +2,13 @@
 
 # There no midstream image currently, fallback to upstream one.
 readonly func_util_image="${KN_PLUGIN_FUNC_UTIL_IMAGE:-quay.io/boson/alpine-socat:1.7.4.3-r1-non-root}"
+readonly default_node_builder_image="${DEFAULT_NODE_BUILDER_IMAGE:-registry.access.redhat.com/ubi8/nodejs-16}"
+readonly default_quarkus_builder_image="${DEFAULT_QUARKUS_BUILDER_IMAGE:-registry.access.redhat.com/ubi8/openjdk-17}"
+readonly default_python_builder_image="${DEFAULT_PYTHON_BUILDER_IMAGE:-registry.access.redhat.com/ubi8/python-39}"
+
 export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
 -X knative.dev/func/pkg/k8s.SocatImage=${func_util_image} \
--X knative.dev/func/pkg/k8s.TarImage=${func_util_image}"
+-X knative.dev/func/pkg/k8s.TarImage=${func_util_image} \
+-X knative.dev/func/pkg/builders/s2i/s2i.DefaultNodeBuilder=${default_node_builder_image} \
+-X knative.dev/func/pkg/builders/s2i/s2i.DefaultQuarkusBuilder=${default_quarkus_builder_image} \
+-X knative.dev/func/pkg/builders/s2i/s2i.DefaultPythonBuilder=${default_python_builder_image}"
