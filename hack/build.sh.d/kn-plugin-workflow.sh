fd03d381b3607ccd7c870ed7575cedffc3c6fc93
Dominik Hanák <domhanak@users.noreply.github.com>
2023-09-26 14:17:38 +0200

    SRVCOM-2673: Update quarkus_version for kn-plugin-workflow (#263)

--- hack/build.sh.d/kn-plugin-workflow.sh
+++ hack/build.sh.d/kn-plugin-workflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-readonly quarkus_version="2.13.8.Final-redhat-00004"
+readonly quarkus_version="2.13.8.SP2-redhat-00001"
 readonly quarkus_platform_group_id="com.redhat.quarkus.platform"
 readonly dev_mode_image="registry.redhat.io/openshift-serverless-1-tech-preview/logic-swf-devmode-rhel8:1.30"
 export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
