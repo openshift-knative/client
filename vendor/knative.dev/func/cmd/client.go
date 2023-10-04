0e8909e7b722e5fc583c0b153bb1c46d4b24adb5
David Simansky <dsimansk@redhat.com>
2023-10-04 10:48:01 +0200

    [release-v1.10] Add func v1.11.1 (#275)
    * [release-v1.10] Add func v1.11.1
    
    * Update vendor dir

---
+++ vendor/knative.dev/func/cmd/client.go
@@ -0,0 +1,162 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"knative.dev/func/cmd/prompt"
+	"knative.dev/func/pkg/builders/buildpacks"
+	"knative.dev/func/pkg/config"
+	"knative.dev/func/pkg/docker"
+	"knative.dev/func/pkg/docker/creds"
+	fn "knative.dev/func/pkg/functions"
+	fnhttp "knative.dev/func/pkg/http"
+	"knative.dev/func/pkg/k8s"
+	"knative.dev/func/pkg/knative"
+	"knative.dev/func/pkg/pipelines/tekton"
+	"knative.dev/func/pkg/progress"
+)
+
+// ClientConfig settings for use with NewClient
+// These are the minimum settings necessary to create the default client
+// instance which has most subsystems initialized.
+type ClientConfig struct {
+	// Namespace in the remote cluster to use for any client commands which
+	// touch the remote.  Optional.  Empty namespace indicates the namespace
+	// currently configured in the client's connection should be used.
+	Namespace string
+
+	// Verbose logging.  By default, logging output is kept to the bare minimum.
+	// Use this flag to configure verbose logging throughout.
+	Verbose bool
+
+	// Allow insecure server connections when using SSL
+	InsecureSkipVerify bool
+}
+
+// ClientFactory defines a constructor which assists in the creation of a Client
+// for use by commands.
+// See the NewClient constructor which is the fully populated ClientFactory used
+// by commands by default.
+// See NewClientFactory which constructs a minimal ClientFactory for use
+// during testing.
+type ClientFactory func(ClientConfig, ...fn.Option) (*fn.Client, func())
+
+// NewTestClient returns a client factory which will ignore options used,
+// instead using those provided when creating the factory.  This allows
+// for tests to create an entirely default client but with N mocks.
+func NewTestClient(options ...fn.Option) ClientFactory {
+	return func(_ ClientConfig, _ ...fn.Option) (*fn.Client, func()) {
+		return fn.New(options...), func() {}
+	}
+}
+
+// NewClient constructs an fn.Client with the majority of
+// the concrete implementations set.  Provide additional Options to this constructor
+// to override or augment as needed, or override the ClientFactory passed to
+// commands entirely to mock for testing. Note the returned cleanup function.
+// 'Namespace' is optional.  If not provided (see DefaultNamespace commentary),
+// the currently configured is used.
+// 'Verbose' indicates the system should write out a higher amount of logging.
+func NewClient(cfg ClientConfig, options ...fn.Option) (*fn.Client, func()) {
+	var (
+		p  = progress.New(cfg.Verbose)               // updates the CLI
+		t  = newTransport(cfg.InsecureSkipVerify)    // may provide a custom impl which proxies
+		c  = newCredentialsProvider(config.Dir(), t) // for accessing registries
+		d  = newKnativeDeployer(cfg.Namespace, cfg.Verbose)
+		pp = newTektonPipelinesProvider(cfg.Namespace, p, c, cfg.Verbose)
+		o  = []fn.Option{ // standard (shared) options for all commands
+			fn.WithVerbose(cfg.Verbose),
+			fn.WithProgressListener(p),
+			fn.WithTransport(t),
+			fn.WithRepositoriesPath(config.RepositoriesPath()),
+			fn.WithBuilder(buildpacks.NewBuilder(buildpacks.WithVerbose(cfg.Verbose))),
+			fn.WithRemover(knative.NewRemover(cfg.Namespace, cfg.Verbose)),
+			fn.WithDescriber(knative.NewDescriber(cfg.Namespace, cfg.Verbose)),
+			fn.WithLister(knative.NewLister(cfg.Namespace, cfg.Verbose)),
+			fn.WithDeployer(d),
+			fn.WithPipelinesProvider(pp),
+			fn.WithPusher(docker.NewPusher(
+				docker.WithCredentialsProvider(c),
+				docker.WithProgressListener(p),
+				docker.WithTransport(t),
+				docker.WithVerbose(cfg.Verbose))),
+		}
+	)
+
+	// Client is constructed with standard options plus any additional options
+	// which either augment or override the defaults.
+	client := fn.New(append(o, options...)...)
+
+	// A deferrable cleanup function which is used to perform any cleanup, such
+	// as closing the transport
+	cleanup := func() {
+		if err := t.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "error closing http transport. %v", err)
+		}
+	}
+
+	return client, cleanup
+}
+
+// newTransport returns a transport with cluster-flavor-specific variations
+// which take advantage of additional features offered by cluster variants.
+func newTransport(insecureSkipVerify bool) fnhttp.RoundTripCloser {
+	return fnhttp.NewRoundTripper(fnhttp.WithInsecureSkipVerify(insecureSkipVerify), fnhttp.WithOpenShiftServiceCA())
+}
+
+// newCredentialsProvider returns a credentials provider which possibly
+// has cluster-flavor specific additional credential loaders to take advantage
+// of features or configuration nuances of cluster variants.
+func newCredentialsProvider(configPath string, t http.RoundTripper) docker.CredentialsProvider {
+	options := []creds.Opt{
+		creds.WithPromptForCredentials(prompt.NewPromptForCredentials(os.Stdin, os.Stdout, os.Stderr)),
+		creds.WithPromptForCredentialStore(prompt.NewPromptForCredentialStore()),
+		creds.WithTransport(t),
+		creds.WithAdditionalCredentialLoaders(k8s.GetOpenShiftDockerCredentialLoaders()...),
+	}
+
+	// Other cluster variants can be supported here
+	return creds.NewCredentialsProvider(configPath, options...)
+}
+
+func newTektonPipelinesProvider(namespace string, progress *progress.Bar, creds docker.CredentialsProvider, verbose bool) *tekton.PipelinesProvider {
+	options := []tekton.Opt{
+		tekton.WithNamespace(namespace),
+		tekton.WithProgressListener(progress),
+		tekton.WithCredentialsProvider(creds),
+		tekton.WithVerbose(verbose),
+		tekton.WithPipelineDecorator(deployDecorator{}),
+	}
+
+	return tekton.NewPipelinesProvider(options...)
+}
+
+func newKnativeDeployer(namespace string, verbose bool) fn.Deployer {
+	options := []knative.DeployerOpt{
+		knative.WithDeployerNamespace(namespace),
+		knative.WithDeployerVerbose(verbose),
+		knative.WithDeployerDecorator(deployDecorator{}),
+	}
+
+	return knative.NewDeployer(options...)
+}
+
+type deployDecorator struct {
+	oshDec k8s.OpenshiftMetadataDecorator
+}
+
+func (d deployDecorator) UpdateAnnotations(function fn.Function, annotations map[string]string) map[string]string {
+	if k8s.IsOpenShift() {
+		return d.oshDec.UpdateAnnotations(function, annotations)
+	}
+	return annotations
+}
+
+func (d deployDecorator) UpdateLabels(function fn.Function, labels map[string]string) map[string]string {
+	if k8s.IsOpenShift() {
+		return d.oshDec.UpdateLabels(function, labels)
+	}
+	return labels
+}
---
+++ vendor/knative.dev/func/cmd/invoke.go
@@ -0,0 +1,411 @@
+package cmd
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/ory/viper"
+	"github.com/spf13/cobra"
+
+	"knative.dev/func/pkg/config"
+	fn "knative.dev/func/pkg/functions"
+	"knative.dev/func/pkg/utils"
+)
+
+func NewInvokeCmd(newClient ClientFactory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "invoke",
+		Short: "Invoke a local or remote function",
+		Long: `
+NAME
+	{{rootCmdUse}} invoke - test a function by invoking it with test data
+
+SYNOPSIS
+	{{rootCmdUse}} invoke [-t|--target] [-f|--format]
+	             [--id] [--source] [--type] [--data] [--file] [--content-type]
+	             [-s|--save] [-p|--path] [-i|--insecure] [-c|--confirm] [-v|--verbose]
+
+DESCRIPTION
+	Invokes the function by sending a test request to the currently running
+	function instance, either locally or remote.  If the function is running
+	both locally and remote, the local instance will be invoked.  This behavior
+	can be manually overridden using the --target flag.
+
+	Functions are invoked with a test data structure consisting of five values:
+		id:            A unique identifier for the request.
+		source:        A sender name for the request (sender).
+		type:          A type for the request.
+		data:          Data (content) for this request.
+		content-type:  The MIME type of the value contained in 'data'.
+
+	The values of these parameters can be individually altered from their defaults
+	using their associated flags. Data can also be provided from a file using the
+	--file flag.
+
+	Invocation Target
+	  The function instance to invoke can be specified using the --target flag
+	  which accepts the values "local", "remote", or <URL>.  By default the
+	  local function instance is chosen if running (see {{rootCmdUse}} run).
+	  To explicitly target the remote (deployed) function:
+	    {{rootCmdUse}} invoke --target=remote
+	  To target an arbitrary endpoint, provide a URL:
+	    {{rootCmdUse}} invoke --target=https://myfunction.example.com
+
+	Invocation Data
+	  Providing a filename in the --file flag will base64 encode its contents
+	  as the "data" parameter sent to the function.  The value of --content-type
+	  should be set to the type from the source file.  For example, the following
+	  would send a JPEG base64 encoded in the "data" POST parameter:
+	    {{rootCmdUse}} invoke --file=example.jpeg --content-type=image/jpeg
+
+	Message Format
+	  By default functions are sent messages which match the invocation format
+	  of the template they were created using; for example "http" or "cloudevent".
+	  To override this behavior, use the --format (-f) flag.
+	    {{rootCmdUse}} invoke -f=cloudevent -t=http://my-sink.my-cluster
+
+EXAMPLES
+
+	o Invoke the default (local or remote) running function with default values
+	  $ {{rootCmdUse}} invoke
+
+	o Run the function locally and then invoke it with a test request:
+	  (run in two terminals or by running the first in the background)
+	  $ {{rootCmdUse}} run
+	  $ {{rootCmdUse}} invoke
+
+	o Deploy and then invoke the remote function:
+	  $ {{rootCmdUse}} deploy
+	  $ {{rootCmdUse}} invoke
+
+	o Invoke a remote (deployed) function when it is already running locally:
+	  (overrides the default behavior of preferring locally running instances)
+	  $ {{rootCmdUse}} invoke --target=remote
+
+	o Specify the data to send to the function as a flag
+	  $ {{rootCmdUse}} invoke --data="Hello World!"
+
+	o Send a JPEG to the function
+	  $ {{rootCmdUse}} invoke --file=example.jpeg --content-type=image/jpeg
+
+	o Invoke an arbitrary endpoint (HTTP POST)
+		$ {{rootCmdUse}} invoke --target="https://my-http-handler.example.com"
+
+	o Invoke an arbitrary endpoint (CloudEvent)
+		$ {{rootCmdUse}} invoke -f=cloudevent -t="https://my-event-broker.example.com"
+
+	o Allow insecure server connections when using SSL
+		$ {{rootCmdUse}} invoke --insecure
+
+`,
+		SuggestFor: []string{"emit", "emti", "send", "emit", "exec", "nivoke", "onvoke", "unvoke", "knvoke", "imvoke", "ihvoke", "ibvoke"},
+		PreRunE:    bindEnv("path", "format", "target", "id", "source", "type", "data", "content-type", "file", "insecure", "confirm", "verbose"),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runInvoke(cmd, args, newClient)
+		},
+	}
+
+	// Config
+	cfg, err := config.NewDefault()
+	if err != nil {
+		fmt.Fprintf(cmd.OutOrStdout(), "error loading config at '%v'. %v\n", config.File(), err)
+	}
+
+	// Flags
+	cmd.Flags().StringP("format", "f", "", "Format of message to send, 'http' or 'cloudevent'.  Default is to choose automatically. ($FUNC_FORMAT)")
+	cmd.Flags().StringP("target", "t", "", "Function instance to invoke.  Can be 'local', 'remote' or a URL.  Defaults to auto-discovery if not provided. ($FUNC_TARGET)")
+	cmd.Flags().StringP("id", "", "", "ID for the request data. ($FUNC_ID)")
+	cmd.Flags().StringP("source", "", fn.DefaultInvokeSource, "Source value for the request data. ($FUNC_SOURCE)")
+	cmd.Flags().StringP("type", "", fn.DefaultInvokeType, "Type value for the request data. ($FUNC_TYPE)")
+	cmd.Flags().StringP("content-type", "", fn.DefaultInvokeContentType, "Content Type of the data. ($FUNC_CONTENT_TYPE)")
+	cmd.Flags().StringP("data", "", fn.DefaultInvokeData, "Data to send in the request. ($FUNC_DATA)")
+	cmd.Flags().StringP("file", "", "", "Path to a file to use as data. Overrides --data flag and should be sent with a correct --content-type. ($FUNC_FILE)")
+	cmd.Flags().BoolP("insecure", "i", false, "Allow insecure server connections when using SSL. ($FUNC_INSECURE)")
+	addConfirmFlag(cmd, cfg.Confirm)
+	addPathFlag(cmd)
+	addVerboseFlag(cmd, cfg.Verbose)
+
+	return cmd
+}
+
+// Run
+func runInvoke(cmd *cobra.Command, args []string, newClient ClientFactory) (err error) {
+	// Gather flag values for the invocation
+	cfg, err := newInvokeConfig(newClient)
+	if err != nil {
+		return
+	}
+
+	// Load the function
+	f, err := fn.NewFunction(cfg.Path)
+	if err != nil {
+		return
+	}
+	if err = f.Validate(); err != nil {
+		fmt.Printf("error validating function at '%v'. %v\n", f.Root, err)
+		return err
+	}
+	if !f.Initialized() {
+		return fn.NewErrNotInitialized(f.Root)
+	}
+
+	// Client instance from env vars, flags, args and user prompts (if --confirm)
+	client, done := newClient(ClientConfig{Namespace: f.Deploy.Namespace, Verbose: cfg.Verbose, InsecureSkipVerify: cfg.Insecure})
+	defer done()
+
+	// Message to send the running function built from parameters gathered
+	// from the user (or defaults)
+	m := fn.InvokeMessage{
+		ID:          cfg.ID,
+		Source:      cfg.Source,
+		Type:        cfg.Type,
+		ContentType: cfg.ContentType,
+		Data:        cfg.Data,
+		Format:      cfg.Format,
+	}
+
+	// If --file was specified, use its content for message data
+	if cfg.File != "" {
+		content, err := os.ReadFile(cfg.File)
+		if err != nil {
+			return err
+		}
+		m.Data = base64.StdEncoding.EncodeToString(content)
+	}
+
+	// Invoke
+	metadata, body, err := client.Invoke(cmd.Context(), cfg.Path, cfg.Target, m)
+	if err != nil {
+		return err
+	}
+
+	// When Verbose
+	// - Print an explicit "Received response" indicator
+	// - Print metadata (headers for HTTP requests, CloudEvents already include
+	//   metadata in their data value.
+	if cfg.Verbose {
+		// Print a "Received response" message because a simple echo to
+		// stdout could be confusing on a first-time run, viewing a proper echo.
+		// user feedback suggests this actually be placed behind the --verbose
+		// setting:
+		fmt.Println("Function invoked.  Response:")
+
+		if len(metadata) > 0 {
+			fmt.Println("  Metadata:")
+		}
+		for k, vv := range metadata {
+			values := strings.Join(vv, ";")
+			fmt.Fprintf(cmd.OutOrStdout(), "    %v: %v\n", k, values)
+		}
+		if len(metadata) > 0 {
+			fmt.Println("  Content:")
+		}
+	}
+
+	// Always print the response's default stringification
+	// Note body already includes a linebreak.
+	fmt.Fprint(cmd.OutOrStdout(), body)
+	return
+}
+
+type invokeConfig struct {
+	Path        string
+	Target      string
+	Format      string
+	ID          string
+	Source      string
+	Type        string
+	Data        string
+	ContentType string
+	File        string
+	Confirm     bool
+	Verbose     bool
+	Insecure    bool
+}
+
+func newInvokeConfig(newClient ClientFactory) (cfg invokeConfig, err error) {
+	cfg = invokeConfig{
+		Path:        viper.GetString("path"),
+		Target:      viper.GetString("target"),
+		Format:      viper.GetString("format"),
+		ID:          viper.GetString("id"),
+		Source:      viper.GetString("source"),
+		Type:        viper.GetString("type"),
+		Data:        viper.GetString("data"),
+		ContentType: viper.GetString("content-type"),
+		File:        viper.GetString("file"),
+		Confirm:     viper.GetBool("confirm"),
+		Verbose:     viper.GetBool("verbose"),
+		Insecure:    viper.GetBool("insecure"),
+	}
+
+	// If file was passed, read it in as data
+	if cfg.File != "" {
+		b, err := os.ReadFile(cfg.File)
+		if err != nil {
+			return cfg, err
+		}
+		cfg.Data = string(b)
+	}
+
+	// if not in confirm/prompting mode, the cfg structure is complete.
+	if !cfg.Confirm {
+		return
+	}
+
+	// If in interactive terminal mode, prompt to modify defaults.
+	if interactiveTerminal() {
+		return cfg.prompt()
+	}
+
+	// Confirming, but noninteractive, is essentially a selective verbose mode
+	// which prints out the effective values of config as a confirmation.
+	fmt.Printf("Path: %v\n", cfg.Path)
+	fmt.Printf("Target: %v\n", cfg.Target)
+	fmt.Printf("ID: %v\n", cfg.ID)
+	fmt.Printf("Source: %v\n", cfg.Source)
+	fmt.Printf("Type: %v\n", cfg.Type)
+	fmt.Printf("Data: %v\n", cfg.Data)
+	fmt.Printf("Content Type: %v\n", cfg.ContentType)
+	fmt.Printf("File: %v\n", cfg.File)
+	fmt.Printf("Insecure: %v\n", cfg.Insecure)
+	return
+}
+
+func (c invokeConfig) prompt() (invokeConfig, error) {
+	var qs []*survey.Question
+
+	// First get path to effective function
+	qs = []*survey.Question{
+		{
+			Name: "Path",
+			Prompt: &survey.Input{
+				Message: "Function Path:",
+				Default: c.Path,
+			},
+			Validate: func(val interface{}) error {
+				if val.(string) != "" {
+					derivedName, _ := deriveNameAndAbsolutePathFromPath(val.(string))
+					return utils.ValidateFunctionName(derivedName)
+				}
+				return nil
+			},
+			Transform: func(ans interface{}) interface{} {
+				if ans.(string) != "" {
+					_, absolutePath := deriveNameAndAbsolutePathFromPath(ans.(string))
+					return absolutePath
+				}
+				return ""
+			},
+		},
+	}
+	if err := survey.Ask(qs, &c); err != nil {
+		return c, err
+	}
+	formatOptions := []string{"", "http", "cloudevent"}
+	qs = []*survey.Question{
+		{
+			Name: "Target",
+			Prompt: &survey.Input{
+				Message: "(Optional) Target ('local', 'remote' or URL).  If not provided, local will be preferred over remote.",
+				Default: "",
+			},
+		},
+		{
+			Name: "Format",
+			Prompt: &survey.Select{
+				Message: "(Optional) Format Override",
+				Options: formatOptions,
+				Default: surveySelectDefault(c.Format, formatOptions),
+			},
+		},
+	}
+	if err := survey.Ask(qs, &c); err != nil {
+		return c, err
+	}
+
+	// Prompt for the next set of values, with defaults set first by the function
+	// as it exists on disk, followed by environment variables, and finally flags.
+	// user interactive prompts therefore are the last applied, and thus highest
+	// precedence values.
+	qs = []*survey.Question{
+		{
+			Name: "ID",
+			Prompt: &survey.Input{
+				Message: "Data ID",
+				Default: c.ID,
+			},
+		}, {
+			Name: "Source",
+			Prompt: &survey.Input{
+				Message: "Data Source",
+				Default: c.Source,
+			},
+		}, {
+			Name: "Type",
+			Prompt: &survey.Input{
+				Message: "Data Type",
+				Default: c.Type,
+			},
+		}, {
+			Name: "File",
+			Prompt: &survey.Input{
+				Message: "(Optional) Load Data Content from File",
+				Default: c.File,
+			},
+		},
+	}
+	if err := survey.Ask(qs, &c); err != nil {
+		return c, err
+	}
+
+	// If the user did not specify a file for data content, prompt for it
+	if c.File == "" {
+		qs = []*survey.Question{
+			{
+				Name: "Data",
+				Prompt: &survey.Input{
+					Message: "Data Content",
+					Default: c.Data,
+				},
+			},
+		}
+		if err := survey.Ask(qs, &c); err != nil {
+			return c, err
+		}
+	}
+
+	// Finally, allow mutation of the data content type.
+	contentTypeMessage := "Content type of data"
+	if c.File != "" {
+		contentTypeMessage = "Content type of file"
+	}
+	qs = []*survey.Question{
+		{
+			Name: "ContentType",
+			Prompt: &survey.Input{
+				Message: contentTypeMessage,
+				Default: c.ContentType,
+			},
+		}}
+	if err := survey.Ask(qs, &c); err != nil {
+		return c, err
+	}
+
+	qs = []*survey.Question{
+		{
+			Name: "Insecure",
+			Prompt: &survey.Confirm{
+				Message: "Allow insecure server connections when using SSL",
+				Default: c.Insecure,
+			},
+		}}
+	if err := survey.Ask(qs, &c); err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
