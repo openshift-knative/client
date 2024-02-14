ea254827b9e8eabf8ac10ace9460d133d85ec57d
David Simansky <dsimansk@redhat.com>
2024-02-14 13:07:19 +0100

    Fix dev image tag

--- hack/build.sh.d/kn-plugin-workflow.sh
+++ hack/build.sh.d/kn-plugin-workflow.sh
@@ -3,7 +3,7 @@
 # Env variables can be injected to override values during build phase
 readonly quarkus_version="${LOGIC_QUARKUS_VERSION:-3.2.9.Final-redhat-00004}"
 readonly quarkus_platform_group_id="${LOGIC_QUARKUS_GROUP:-com.redhat.quarkus.platform}"
-readonly dev_mode_image="${LOGIC_DEV_MODE_IMAGE:-registry.redhat.io/openshift-serverless-1-tech-preview/logic-swf-devmode-rhel8:1.32}"
+readonly dev_mode_image="${LOGIC_DEV_MODE_IMAGE:-registry.redhat.io/openshift-serverless-1-tech-preview/logic-swf-devmode-rhel8:1.32.0}"
 
 export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
 -X github.com/apache/incubator-kie-tools/packages/kn-plugin-workflow/pkg/metadata.DevModeImage=${dev_mode_image} \
