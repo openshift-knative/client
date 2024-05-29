aeedf5adad9cdfcd2c5e6e12a796fe9e6dc5396a
David Simansky <dsimansk@redhat.com>
2024-05-29 15:04:38 +0200

    Add plugin install command (#379)
    * Add plugin install command
    
    * Split into functions and cleanup code
    
    * Fix formatting of desc
    
    * Add plugin remove and update flag
    
    * Update pkg/kn/commands/plugin/install.go
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * Update pkg/kn/commands/plugin/remove.go
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * Update pkg/kn/commands/plugin/remove.go
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * Update pkg/kn/commands/plugin/remove.go
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * Update pkg/kn/commands/plugin/remove.go
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>
    
    * Remove misleading alias cmd
    
    ---------
    
    Co-authored-by: Roland Huß <rhuss@redhat.com>

---
+++ pkg/kn/commands/plugin/remove.go
@@ -0,0 +1,52 @@
+// Copyright © 2024 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plugin
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+	"knative.dev/client/pkg/kn/commands"
+	"knative.dev/client/pkg/kn/config"
+)
+
+// NewPluginRemoveCommand creates a new `kn plugin remove` command
+func NewPluginRemoveCommand(p *commands.KnParams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "remove",
+		Short: "remove a plugin",
+		Long: `Remove a plugin.
+
+Plugin is removed from ${KN_CONFIG_DIR}/kn/plugins.
+
+Current: ` + config.GlobalConfig.PluginsDir(),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("'kn plugin remove' requires plugin name")
+			}
+			name := args[0]
+			filename := filepath.Join(config.GlobalConfig.PluginsDir(), name)
+			if _, err := os.Stat(filename); os.IsNotExist(err) {
+				return errors.New("plugin '" + name + "' does not exist")
+			}
+			println("Removed plugin '" + filename + "'")
+			return os.Remove(filename)
+		},
+	}
+	return cmd
+}
